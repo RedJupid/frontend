17f2f681715314246ad94c373b05e8899611e6ae
xd <627393061@qq.com>
2019-12-19 23:37:19 +0800

    主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+const Login = () => import('../views/login/Login.vue')
+const About = () => import('../views/About.vue')
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/about',
+        name: 'about',
+        component: About
+      }
+    ]
   },
   {
     path: '/about',
@@ -16,7 +24,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
@@ -24,6 +37,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  //没有登录时返回登录页面
+  if (localStorage.getItem("loginInfo")==null && to.name !=='login'){
+    next({ name: 'login' })
+  }else{
+    next()
+  }
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider :trigger="null" collapsible v-model="collapsed" :style="{background:'#fff'}">
+      <div class="logo">
+        <img src="../../src/assets/logo.png" style="height:100%">
+      </div>
+      <a-menu
+              mode="inline"
+              :defaultSelectedKeys="['1']"
+              :defaultOpenKeys="['sub1']"
+              :style="{ borderRight: 0}"
+      >
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user" />subnav 1</span>
+          <a-menu-item key="1"><router-link to="/about">About</router-link></a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="laptop" />subnav 2</span>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <span slot="title"><a-icon type="notification" />subnav 3</span>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+                class="trigger"
+                :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                @click="()=> collapsed = !collapsed"
+        />
+      </a-layout-header>
+      <a-layout-content
+              :style="{ margin: '5px 5px 0 5px', padding: '24px', background: '#fff', minHeight:'800px'}"
+      >
+        Content
+        <router-view/>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center; padding:5px 5px">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        collapsed: false,
+      };
+    },
+  };
 </script>
+<style>
+  #components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+
+  #components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+  }
+
+  #components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
+</style>
