4e285915090d0b00bddb96caf5eb34eece445099
xd <627393061@qq.com>
2020-01-01 15:32:31 +0800

    分页

--- src/views/system/User.vue
+++ src/views/system/User.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
     用户管理
-  <a-table :columns="columns" :dataSource="data">
-    <a slot="name" slot-scope="text" href="javascript:;">{{text}}</a>
-<!--    <span slot="customTitle"><a-icon type="smile-o" /> Name</span>-->
+    <a-row>
+      <a-col :span="20">
+        <a-form layout="inline">
+          <a-form-item
+                label="姓名"
+          >
+            <a-input/>
+          </a-form-item>
+        </a-form>
+      </a-col>
+      <a-col :span="4">
+        <a-button>
+          <a-icon type="search" />
+        </a-button>
+      </a-col>
+    </a-row>
+
+
+
+  <a-table :columns="columns" :dataSource="data" :pagination="false">
     <span slot="status" slot-scope="status">
       <a-tag
               :color="status==='可用' ? 'green' : 'red'"
@@ -12,14 +29,15 @@
         {{status}}
       </a-tag>
     </span>
-    <span slot="action" slot-scope="text, record">
-      <a href="javascript:;">Invite 一 {{record.name}}</a>
-      <a-divider type="vertical" />
-      <a href="javascript:;">Delete</a>
-      <a-divider type="vertical" />
-      <a href="javascript:;" class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+    <span slot="action" slot-scope="text">
+      <span style="cursor:pointer; margin-left: 6px; padding: 5px; color: #84cf46"><a-icon type="eye"/></span>
+      <span style="cursor:pointer; margin-left: 6px; padding: 5px; color: #1890ff"><a-icon type="edit"/></span>
+      <span style="cursor:pointer; margin-left: 6px; padding: 5px; color: #f5626a"><a-icon type="delete" /></span>
     </span>
   </a-table>
+    <div style="margin-top: 10px; float: right">
+      <a-pagination showQuickJumper :defaultCurrent="2" :total="100" @change="onChange" :showTotal="total => `Total ${total} items`" />
+    </div>
   </div>
 </template>
 <script>
@@ -51,7 +69,7 @@
       scopedSlots: { customRender: 'status' },
     },
     {
-      title: 'Action',
+      title: '操作',
       key: 'action',
       scopedSlots: { customRender: 'action' },
     },
@@ -63,7 +81,13 @@
       return {
         data,
         columns,
+        current:1,
       };
+    },
+    methods:{
+      onChange(pageNumber) {
+        console.log('Page: ', pageNumber);
+      }
     },
     created() {
       this.axios.get('/sys/user/list').then(res=>{
