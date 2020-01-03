f45c7a039c0452d92c4299156373616434978b50
xd <627393061@qq.com>
2020-01-03 19:46:37 +0800

    菜单管理

--- src/views/system/Menu.vue
+++ src/views/system/Menu.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>
-    <h1>菜单管理</h1>
-  </div>
+  <a-tree
+          checkable
+          @expand="onExpand"
+          :expandedKeys="expandedKeys"
+          :autoExpandParent="autoExpandParent"
+          v-model="checkedKeys"
+          @select="onSelect"
+          :selectedKeys="selectedKeys"
+          :treeData="treeData"
+  />
+
 </template>
+<script>
+  const treeData = [];
+
+  export default {
+    data() {
+      return {
+        expandedKeys: ['0-0-0', '0-0-1'],
+        autoExpandParent: true,
+        checkedKeys: ['0-0-0'],
+        selectedKeys: [],
+        treeData,
+      };
+    },
+    watch: {
+      checkedKeys(val) {
+        console.log('onCheck', val);
+      },
+    },
+    methods: {
+      onExpand(expandedKeys) {
+        console.log('onExpand', expandedKeys);
+        // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+        // or, you can remove all expanded children keys.
+        this.expandedKeys = expandedKeys;
+        this.autoExpandParent = false;
+      },
+      onCheck(checkedKeys) {
+        console.log('onCheck', checkedKeys);
+        this.checkedKeys = checkedKeys;
+      },
+      onSelect(selectedKeys, info) {
+        console.log('onSelect', info);
+        this.selectedKeys = selectedKeys;
+      },
+    },
+    created() {
+      this.axios.get('/sys/menu/getTree').then(res=>{
+        let data = res.data.data;
+        console.log(data);
+        this.treeData = data;
+        // this.data = data;
+      })
+    }
+  };
+</script>
