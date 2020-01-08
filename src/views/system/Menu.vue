f30f781c9dc4880a75ab11037f4123b98b58007f
xd <627393061@qq.com>
2020-01-08 20:43:16 +0800

    菜单管理

--- src/views/system/Menu.vue
+++ src/views/system/Menu.vue
@@ -2,6 +2,8 @@
   <div>
     <a-row>
       <a-col :span="12">
+        <a-button><a-icon type="plus" /></a-button>
+        <a-button><a-icon type="sync" /></a-button>
         <a-tree
                 checkable
                 @expand="onExpand"
@@ -17,34 +19,34 @@
       <a-col :span="12">
         <a-form :form="form">
           <a-form-item label="id" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <span>{{menuData.id}}</span>
+            <a-input v-decorator="['id']"/>
           </a-form-item>
           <a-form-item label="上级id" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.parentId"/>
+            <a-input v-decorator="['parentId']"/>
           </a-form-item>
           <a-form-item label="名称" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.name"/>
+            <a-input v-decorator="['name']"/>
           </a-form-item>
           <a-form-item label="类型" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-radio-group name="radioGroup" v-model="menuData.type" :defaultValue="0">
+            <a-radio-group name="radioGroup" v-decorator="['type',{initialValue:0}]">
               <a-radio :value="0">菜单</a-radio>
               <a-radio :value="1">按钮</a-radio>
             </a-radio-group>
           </a-form-item>
           <a-form-item label="图标" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.icon"/>
+            <a-input v-decorator="['icon']"/>
           </a-form-item>
           <a-form-item label="URL" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.url"/>
+            <a-input v-decorator="['url']"/>
           </a-form-item>
           <a-form-item label="权限" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.perms"/>
+            <a-input v-decorator="['perms']"/>
           </a-form-item>
           <a-form-item label="排序" :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
-            <a-input :value="menuData.orderNum"/>
+            <a-input v-decorator="['orderNum']"/>
           </a-form-item>
           <a-form-item>
-            <a-button type="primary" @click="updateMenu">保存</a-button>
+            <a-button type="primary" @click="updateMenu">更新</a-button>
           </a-form-item>
         </a-form>
       </a-col>
@@ -54,9 +56,6 @@
 </template>
 <script>
   const treeData = [];
-
-  const plainOptions = ['Apple', 'Pear', 'Orange'];
-
 
   const formItemLayout = {
     labelCol: { span: 4 },
@@ -73,13 +72,13 @@
         autoExpandParent: true,
         checkedKeys: [],
         selectedKeys: [],
-        treeData,
+        menu:{},//菜单数据
+        treeData,//所有菜单数据
         replaceFields:{
           title:'name',
           key:'id'
         },
         formItemLayout,
-        menuData:{}
       };
     },
     watch: {
@@ -99,16 +98,41 @@
         console.log('onCheck', checkedKeys);
         this.checkedKeys = checkedKeys;
       },
+      //设置菜单的属性值
+      setMenu(menu){
+        this.form.setFieldsValue({
+          'id':menu.id,
+          'parentId':menu.parentId,
+          'name':menu.name,
+          'type':menu.type,
+          'icon':menu.icon,
+          'url':menu.url,
+          'perms':menu.perms,
+          'orderNum':menu.orderNum
+        });
+      },
       onSelect(selectedKeys, info) {
-        // console.log('onSelect', info);
-        let menu = info.selectedNodes[0].data.props;
-        console.log(menu);
-        this.menuData = menu;
+        this.menu = info.selectedNodes[0].data.props;
+        console.log(this.menu);
+        this.setMenu(this.menu);
         this.selectedKeys = selectedKeys;
       },
       updateMenu(){
-        console.log("updatemenu");
-        console.log(this.form.getFieldValue("parentId"));
+        this.axios.put('/sys/menu',{
+          'id':this.form.getFieldValue('id'),
+          'parentId':this.form.getFieldValue('parentId'),
+          'name':this.form.getFieldValue('name'),
+          'type':this.form.getFieldValue('type'),
+          'icon':this.form.getFieldValue('icon'),
+          'url':this.form.getFieldValue('url'),
+          'perms':this.form.getFieldValue('perms'),
+          'orderNum':this.form.getFieldValue('orderNum')
+        }).then(res=>{
+          console.log(res.data);
+        })
+      },
+      addMenu(){
+        console.log("addmenu");
       }
     },
     created() {
@@ -116,7 +140,6 @@
         let data = res.data.data;
         console.log(data);
         this.treeData = data;
-        // this.data = data;
       })
     }
   };
